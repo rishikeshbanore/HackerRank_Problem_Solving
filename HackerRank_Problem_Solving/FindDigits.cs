600843ff6562ed514667b03332ada217894aaa4f
Rishikesh BANORE <R.Banore@iaea.org>
2022-03-10 23:15:16 +0100

    Adding solution for "Find Digit" Problem on Hacker Rank.

---
+++ HackerRank_Problem_Solving/FindDigits.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_Problem_Solving
+{
+    class FindDigits
+    {
+        public static int findDigits(int n)
+        {
+            int No_Of_Divisors = 0;
+            int original_Number =n;
+            int NummberDivide = n;
+
+            if (n < 10)
+            {
+                No_Of_Divisors = 1;
+
+                return No_Of_Divisors;
+            }
+            else
+            {
+                while (NummberDivide % 10 != 0 || NummberDivide / 10 > 0)
+                {
+                    int Divisor = NummberDivide % 10;
+
+                    if (Divisor!=0)
+                    {
+                        if (original_Number % Divisor == 0)
+                        {
+                            No_Of_Divisors++;
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("skipping to next number");
+                    }
+
+                    NummberDivide = NummberDivide / 10;
+                }
+            }
+
+            return No_Of_Divisors;
+        }
+    }
+}
