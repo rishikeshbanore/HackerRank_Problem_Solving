5eb213b791d3718c663b0576b00b7f9b4a73604d
Rishikesh BANORE <R.Banore@iaea.org>
2022-03-10 23:17:29 +0100

    Added Comments for the Problem Statements.

--- HackerRank_Problem_Solving/Program.cs
+++ HackerRank_Problem_Solving/Program.cs
@@ -7,7 +7,7 @@ namespace HackerRank_Problem_Solving
     {
         static void Main(string[] args)
         {
-            //Problem 1:
+            ////Problem 1:
             /*You are choreographing a circus show with various animals. 
                For one act, you are given two kangaroos on a number line ready to jump 
                in the positive direction (i.e, toward positive infinity).
@@ -16,24 +16,46 @@ namespace HackerRank_Problem_Solving
                You have to figure out a way to get both kangaroos at the same location at the same time as part of the show.
                If it is possible, return YES, otherwise return NO.*/
 
-            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            ////TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+            //string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
 
-            int x1 = Convert.ToInt32(firstMultipleInput[0]);
+            //int x1 = Convert.ToInt32(firstMultipleInput[0]);
 
-            int v1 = Convert.ToInt32(firstMultipleInput[1]);
+            //int v1 = Convert.ToInt32(firstMultipleInput[1]);
 
-            int x2 = Convert.ToInt32(firstMultipleInput[2]);
+            //int x2 = Convert.ToInt32(firstMultipleInput[2]);
 
-            int v2 = Convert.ToInt32(firstMultipleInput[3]);
+            //int v2 = Convert.ToInt32(firstMultipleInput[3]);
 
-            string result = Number_Line_Jumps.kangaroo(x1, v1, x2, v2);
+            //string result = Number_Line_Jumps.kangaroo(x1, v1, x2, v2);
 
-            Console.WriteLine(result);
+            //Console.WriteLine(result);
+
+            ////textWriter.Flush();
+            ////textWriter.Close();
+
+            ////Problem 2:
+            /*An integer  is a divisor of an integer  if the remainder of .
+              Given an integer, for each digit that makes up the integer determine whether it is a divisor.
+              Count the number of divisors occurring within the integer.*/
+
+            ////TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+                int result = FindDigits.findDigits(n);
+
+                Console.WriteLine(result);
+            }
 
             //textWriter.Flush();
             //textWriter.Close();
+
         }
     }
 }
